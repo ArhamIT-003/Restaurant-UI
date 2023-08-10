40299affa3fb80697c8fc96eae2706470915d02b
Arham <muhammadarham59@gmail.com>
2023-08-09 17:36:14 -0700

    New branch chaining

---
+++ src/app/api/products/route.ts
@@ -0,0 +1,24 @@
+import { prisma } from "@/utils/connect";
+import { NextRequest, NextResponse } from "next/server";
+
+export const GET = async (req: NextRequest) => {
+  const { searchParams } = new URL(req.url);
+  const cat = searchParams.get("cat");
+
+  console.log(cat);
+
+  try {
+    const product = await prisma.product.findMany({
+      where: {
+        ...(cat ? { catSlug: cat } : { isFeatured: true }),
+      },
+    });
+    return new NextResponse(JSON.stringify(product), { status: 200 });
+  } catch (err) {
+    console.log(err);
+
+    return new NextResponse(JSON.stringify("Something went wrong"), {
+      status: 502,
+    });
+  }
+};
